761901aea004e964973891c04d2adf795133fb63
Josh Endemann <jahend44@gmail.com>
2025-02-25 19:43:47 -0600

    Update page.js
    Working on  Home page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,25 @@
-import Image from "/title.png";
+"use client";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1> Welcome to My Dungeon Mind</h1>
-      <p>Please sign up or log in to get started!</p>
-      <Image src={Image} alt="Title Image" />
+    <div className="text-center p-12 bg-gray-900">
+      <h1 className="text-warmgray-100 text-4xl p-8">
+        {" "}
+        Welcome to My Dungeon Mind
+      </h1>
+      <p className="text-emerald-600 text-xl">
+        Please sign up or log in to get started!
+      </p>
+      <div className="space-x-8 text-xl p-8 text-stone-600 ">
+        <Link href="login" className=" hover:text-emerald-600">
+          Login
+        </Link>
+        <Link href="signup" className=" hover:text-emerald-600">
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 }
